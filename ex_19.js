dd93bc61b4599bb9ac6a3a4a1273765b282611ed
audwnzhfldk@naver.com <audwnzhfldk@naver.com>
2018-12-28 17:47:15 +0900

    object, for in

---
+++ ex_19.js
@@ -0,0 +1,32 @@
+//객체 선언
+//json과 비슷
+var obj = {name : "bmj", age : 23}
+console.log(obj["name"]);
+console.log(obj["age"]);
+console.log(obj.name);
+console.log(obj.age);
+
+//문자열로 처리해 줘야 결과값이 정상적으로 나온다.
+var myFriend = {key: "value", addition : [{name:'PhoMein'},{number:13579}]};
+console.log(myFriend.key);
+console.log(myFriend["key"]);
+
+
+//객체안에 있는 객체를 참조
+console.log(myFriend.addition[0]);
+console.log(myFriend.addition[0].name);
+console.log(myFriend.addition[1]);
+console.log(myFriend.addition.length);
+
+
+//value값이 결과로 나온다.
+console.log("for문 key");
+for(key in myFriend){
+  console.log(myFriend[key]);
+}
+
+//key값이 결과로 나온다.
+console.log("for문 value");
+for(value in myFriend){
+  console.log(value);
+}
