9df75c3d50cd60ea8d6c660e6b2c54eee070e8f3
audwnzhfldk@naver.com <audwnzhfldk@naver.com>
2018-12-28 18:19:42 +0900

    not success....ㅠㅠ

---
+++ 실습1.js
@@ -0,0 +1,42 @@
+
+const widget = {
+    "debug": "on",
+    "window": {
+        "title": "Sample Konfabulator Widget",
+        "name": "main_window",
+        "width": 500,
+        "height": 500
+    },
+    "image": {
+        "src": "Images/Sun.png",
+        "name": "sun1",
+        "hOffset": 250,
+        "vOffset": 250,
+        "alignment": "center"
+    },
+    "text": {
+        "data": "Click Here",
+        "size": 36,
+        "style": "bold",
+        "name": "text1",
+        "hOffset": 250,
+        "vOffset": 100,
+        "alignment": "center",
+        "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"
+    }
+}
+
+
+var list = []; //push로 add
+
+Object.keys(widget).forEach(function (key) {
+  console.log("key");
+  console.log(widget[key]);
+
+  if(typeof(Object[key]) === 'number')
+  {
+    list.push(key);
+  }
+});
+
+console.log(list);
